13d032ea29d4b65ea44a498e91df403ba0d3c23c
walz0 <aidan.m.walz@gmail.com>
2022-04-27 01:58:29 -0400

    Major refactor, added title page feature

--- papel.py
+++ papel.py
@@ -1,29 +1,24 @@
 """
-
     papel - raw text document router
-
-    Written by Aidan Walz 2020
-
+    :: walz 2020
 """
 import sys
 import os
+
 # Output HTML document
 output = ""
-
 # List of paragraphs
 paragraphs = []
-
 # List of citations
 citations = []
-
+# Title page lines
+title_lines = []
 # Number of spaces per tab
 tab_stop = 4
-
 
 def init():
     global output
     # DEFAULTS
-
     options = {
         '-s' : 17, # font-size
         '-i' : 50, # text-indent 
@@ -33,24 +28,27 @@ def init():
         for o in options:
             if o in sys.argv:
                 options[o] = select_args(o)
-
-    output += "<!DOCTYPE html><html><style>body {font-size: " + str(options['-s'])
-    output += "px; text-indent: " + str(options['-i']) 
-    output += "px; line-height: " + str(options['-h']) + "}"
-    output += ".citation { padding-left: 36px; text-indent: -36px }</style><body>"
+    
+    # This should be loaded from a css file
+    output += "<!DOCTYPE html><html>"
+    output += "<style>"
+    output += "body {font-size: %spx; " % (str(options['-s']))
+    output += "text-indent: %spx; " % (str(options['-i']))
+    output += "line-height: %s}" % (str(options['-h']))
+    output += ".citation { padding-left: 36px; text-indent: -36px }"
+    output += ".title-page { line-height: 1.5; text-align: center }"
+    output += "</style>"
+    output += "<body>"
     pass
-
 
 def select_args(option):
     """
-
         ARGUMENTS:
             -s :: font-size
             -i :: text-indent 
             -h :: line-height 
-
     """
-     # Screen args for selected option
+    # Screen args for selected option
     for i in range(1, len(sys.argv)):
         if sys.argv[i] == option: # If option selected 
             if i != len(sys.argv) - 1: # If not at the end of args
@@ -61,6 +59,129 @@ def select_args(option):
                 except:
                     print("ERROR: Argument not valid, please use the syntax \"{} [integer]\"".format(option))
 
+def addTitlePage(raw, nl):
+    global output
+    global title_lines
+    # Start of the title page
+    t_index = 0
+    for i in range(len(raw)):
+        if raw[i] == "\\":
+            if (i - 1) in nl: # If a return precedes
+                raw = raw[:i] # Remove indicator in output
+                t_index = i # Record start of title page
+                raw = raw.strip()
+                break
+    # Clear newlines
+    nl = [] 
+    if t_index: # If title page exists
+        # Previous line start index
+        prev = 0 
+        # Crop raw text to title page section
+        raw = raw[:t_index]
+        # Count new lines
+        for i in range(len(raw)):
+            if raw[i] == "\n":
+                nl += [i] 
+
+        # Add padding to top
+        output += "<br>" * 6
+        output += "<div class='title-page'>"
+        for i in range(len(raw)):
+            # If new line
+            if i != 0:
+                if i in nl and i+1 in nl: # If new line is consecutive
+                    if len(title_lines) == 0:
+                        # Center section
+                        # Add title
+                        output += "<p style='padding-left: 20px; text-align: center; text-indent: 0; font-weight: bold;'>{}</p><br>".format(raw[prev:i].strip())
+                        title_lines += [raw[prev:i].strip()]
+                        prev = i
+                        continue
+                    # Add new line
+                    addTitlePageLine(raw[prev:i].strip())
+                    prev = i
+    output += "</div>"
+    # Add padding to bottom
+    output += "<br>" * 9
+
+# Returns a list of indeces of the \n chars in the text
+def getNewlines(text):
+    # Output
+    nl = []
+    # Previous paragraph start index
+    prev = 0
+    # Calculate end index by distance from last newline 
+    for i in range(len(text)):
+        if text[i] == "\n":
+            nl += [i]
+    return nl
+
+def addBody(raw, nl):
+    t_index = 0 # Title page index
+    for i in range(len(raw)):
+        if raw[i] == "\\":
+            if (i - 1) in nl:
+                # Detected title page
+                t_index = i+2
+    #nl = []
+    # Previous paragraph start index
+    prev = t_index
+    # Calculate end index by distance from last newline 
+    for i in range(t_index, len(raw)):
+        if raw[i] == "\n":
+            #nl += [i]
+            if i == len(raw) - 1:
+                addParagraph(raw[prev:i].strip())
+                break
+            # If new line is consecutive
+            if raw[i - 1] == "\n":
+                # Add a new paragraph
+                addParagraph(raw[prev:i].strip())
+                prev = i
+        # References reached
+        if raw[i] == "/":
+            if (i - 1) in nl:
+                addParagraph(raw[prev:i].strip())
+                break
+        if i == len(raw) - 2:
+            # Reached end of document
+            addParagraph(raw[prev:i].strip())
+            break
+
+def addReferences(raw, nl):
+    global output
+    # Start of References
+    r_index = 0 
+    # References (Forward slash between two returns)
+    for i in range(len(raw)):
+        if raw[i] == "/":
+            if (i - 1) in nl: # If a return precedes
+                raw = raw[:i] + raw[i+1:] # Remove indicator in output
+                r_index = i # Record start of References
+                break
+    # Clear newlines
+    nl = [] 
+    if r_index: # If References exists
+        # Add References title
+        output += "<p style='text-align: center; text-indent: 0; font-weight: bold; margin: 35px 0 -10px 0'>References</p>"  
+        # Previous citation start index
+        prev = 0 
+        # Crop raw text to References section
+        raw = raw[r_index:]
+        # Count new lines
+        for i in range(len(raw)):
+            if raw[i] == "\n":
+                nl += [i] 
+        # Remove new line after indicator
+        nl = nl[1:]
+        
+        for i in range(len(raw)):
+            # If new line (citation)
+            if i != 0:
+                if i in nl and i+1 in nl: # If new line is consecutive
+                    # Add new citation
+                    addCitation(raw[prev:i])
+                    prev = i
 
 def addParagraph(text):
     global output
@@ -68,13 +189,17 @@ def addParagraph(text):
     paragraphs += [text]
     output += "<p>{}</p>".format(text)
 
-
 def addCitation(text):
     global output
     global citations
     output += "<p class='citation'>{}</p>".format(text)
     citations += [text]
-    
+
+def addTitlePageLine(text):
+    global output
+    global title_lines
+    output += "<p>{}</p>".format(text)
+    title_lines += [text]
 
 def spacesToTabs(text):
     global tab_stop
@@ -87,7 +212,6 @@ def spacesToTabs(text):
                 text = text[:i] + '\t' + text[i + (tab_stop):]
     return text
                 
-                
 def render():
     global output
     # Write end of document
@@ -99,7 +223,6 @@ def render():
     # Close file
     f.close()
 
-
 if __name__ == "__main__":
     # Load raw text 
     raw = ""
@@ -108,70 +231,22 @@ if __name__ == "__main__":
 
     # Initialize HTML document
     init()
-
     # New line character queue
-    nl = []
-    # Previous paragraph start index
-    prev = 0
-    # Calculate end index by distance from last newline 
-    for i in range(len(raw)):
-        if raw[i] == "\n":
-            nl += [i]
-            if i == len(raw) - 1:
-                addParagraph(raw[prev:i])
-                break
-        # If new line is consecutive
-        if len(nl) > 1:
-            if nl[len(nl) - 2] == i - 1:
-                # Add a new paragraph
-                addParagraph(raw[prev:i])
-                prev = i
-        # Works Cited reached
-        if raw[i] == "/":
-            if (i - 1) in nl:
-                addParagraph(raw[prev:i])
-                break
-        if i == len(raw) - 2:
-            # Reached end of document
-            addParagraph(raw[prev:i])
-            break
- 
-    # Start of Works Cited 
-    wc_index = 0 
-    # Works Cited (Forward slash between two returns)
-    for i in range(len(raw)):
-        if raw[i] == "/":
-            if (i - 1) in nl: # If a return precedes
-                raw = raw[:i] + raw[i+1:] # Remove indicator in output
-                wc_index = i # Record start of Works Cited
-                for _ in nl:
-                    if _ == wc_index - 1:
-                        nl = nl[_:] # Crop nl to start from Works Cited
-                break
-    
-    if wc_index > 0: # If Works Cited exists
-        # Add Works Cited title
-        output += "<p style='text-align: center; text-indent: 0; font-weight: bold; margin: 35px 0 -10px 0'>References</p>"  
-        # Previous citation start index
-        prev = 0 
-        # Crop raw text to works cited section
-        raw = raw[wc_index:]
-        # Count new lines
-        for i in range(len(raw)):
-            if raw[i] == "\n":
-                nl += [i] 
-        # Remove new line after indicator
-        nl = nl[1:]
-        
-        for i in range(len(raw)):
-            # If new line (citation)
-            if i != 0:
-                if i in nl and i+1 in nl: # If new line consecutive
-                    # Add new citation
-                    addCitation(raw[prev:i])
-                    prev = i 
+    nl = getNewlines(raw)
+    # Process title page
+    addTitlePage(raw, nl)
+    if len(title_lines[0].split(" ")) > 12:
+        print("Warning: APA recommends that your title be no more than 12 words in length")
+    # Process body
+    addBody(raw, nl)
+    # Process references
+    addReferences(raw, nl)
+    # Output HTML file
     render()
 
     """
-        Fix links in citations zooming out the page
+        -Footers
+        -Page Numbers
+        -Save as PDF (use selenium to automate chrome print to PDF with 1" margins)
+        -Fix links in citations zooming out the page
     """
